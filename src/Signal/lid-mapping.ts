6e4bbf291305e6eb9f70454a7d9c9d97a5f86f70
Ilya Borodin <ilya.borodin8@gmail.com>
2025-10-10 12:44:47 -0300

    Changed ttl time

--- src/Signal/lid-mapping.ts
+++ src/Signal/lid-mapping.ts
@@ -5,7 +5,7 @@ import { isJidHostedLidUser, isJidHostedPnUser, isLidUser, isPnUser, jidDecode, 
 
 export class LIDMappingStore {
 	private readonly mappingCache = new LRUCache<string, string>({
-		ttl: 7 * 24 * 60 * 60 * 1000, // 7 days
+		ttl: 60 * 60 * 1000, // 1 h
 		ttlAutopurge: true,
 		updateAgeOnGet: true
 	})
